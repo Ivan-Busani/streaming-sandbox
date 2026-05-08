fc658bb65c12497c8eeefaae7b01f646c679b78b
Ivan Busani <ivan_fcambric@hotmail.com>
2026-05-08 15:59:55 -0600

    Opt-in UnstableApi; update raw playlist
    Add @OptIn(UnstableApi::class) and import to DefaultPlayerEngineFactory to acknowledge Media3 unstable API usage and avoid related warnings. Update app/src/main/res/raw/mx_live_streams_playlist.m3u with numerous new and replaced channel entries, updated logos and stream URLs, and cleanup of several existing items to refresh the playlist content.

--- app/src/main/java/com/mivan/streamingsandbox/feature/player/data/DefaultPlayerEngineFactory.kt
+++ app/src/main/java/com/mivan/streamingsandbox/feature/player/data/DefaultPlayerEngineFactory.kt
@@ -1,5 +1,7 @@
 package com.mivan.streamingsandbox.feature.player.data
 
+import androidx.annotation.OptIn
+import androidx.media3.common.util.UnstableApi
 import com.mivan.streamingsandbox.feature.player.domain.PlayerEngine
 import com.mivan.streamingsandbox.feature.player.domain.PlayerEngineFactory
 import com.mivan.streamingsandbox.feature.player.domain.PlayerVendor
@@ -7,7 +9,8 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class DefaultPlayerEngineFactory @Inject constructor(
+class DefaultPlayerEngineFactory @OptIn(UnstableApi::class)
+@Inject constructor(
     private val exoPlayerEngine: ExoPlayerEngine,
     private val bitmovinPlayerEngine: BitmovinPlayerEngine,
     private val castlabsPlayerEngine: CastlabsPlayerEngine
